cc6fd936bf068c19e488f5a6be25589041a89afe
xin du <xin.du@ninebot.com>
2018-03-12 19:54:52 +0800

    clip

--- src/process.js
+++ src/process.js
@@ -3,12 +3,14 @@ var exec = require('child_process').exec;
 
 var respone = {};
 var mPid;
+var mTempTag = "xxx"
 
 function readLine(pid, callback) {
   mPid = pid;
   var rl = readline.createInterface(process.stdin, null);
   rl.on('line', function(line) {
-    if (mPid === "") return;
+    if (mPid === "")
+      return;
     var lines = line.split(/\s+/);
     var l = lines[4];
     var p = lines[2];
@@ -18,7 +20,8 @@ function readLine(pid, callback) {
     }
     if (l && f) {
       respone.tag = l;
-      respone.line = line;
+      var tLine = clipping(line, lines[5]);
+      respone.line = tLine;
       callback(respone);
     }
   }).on('close', function() {
@@ -26,7 +29,41 @@ function readLine(pid, callback) {
   });
 }
 
-function setPid(pid){
+function clipping(line, tag, tid) {
+  if (!tid) tid = "";
+  var tLine;
+  var i = line.indexOf(tag);
+  if (mTempTag == tag) {
+    tLine = line.substring(i, line.length)
+    tLine = new Array(i + 1 - tid.length).join(" ") + tLine;
+  } else {
+    tLine = line;
+  }
+  mTempTag = tag;
+  return tLine;
+}
+
+function removeDate(line, date) {
+  var i = date.length;
+  var tLine = line.substring(i, line.length)
+  return tLine.trim();
+}
+
+function removePid(line, pid) {
+  var i = line.indexOf(pid)
+  var startLine = line.substring(0, i);
+  var endLine = line.substring(i + pid.length, line.length);
+  return startLine.trim() + endLine;
+}
+
+function removeTid(line, tid) {
+  var i = line.indexOf(tid)
+  var startLine = line.substring(0, i);
+  var endLine = line.substring(i + tid.length, line.length);
+  return startLine.trim() + endLine;
+}
+
+function setPid(pid) {
   mPid = pid;
 }
 
